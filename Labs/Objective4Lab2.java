c3ce6e1e1c612afc31b42423a40530548275626a
Sarocha <rocha.right007@gmail.com>
2024-05-22 14:26:10 -0600

    complete added close()

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -31,5 +31,9 @@ public class Objective4Lab2{
 
 		System.out.println("The sum of " + num1 + " + " + num2 + " + " + num3 + " = " + sumint);
 		System.out.println("The sum of " + dub1 + " + " + dub2 + " + " + dub3 + " = " + sumdub);
+
+		keyboard.close();
+			
+		
 	}
 }
