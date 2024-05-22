ea125837108c5b7935cc8841926c81880d733509
Sarocha <rocha.right007@gmail.com>
2024-05-22 14:22:56 -0600

    complete Obejective4lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+ public class Objective4Lab3{
+ 	public static void main(String[] args){
+ 		int birthYear, age, currentYear;
+
+ 		birthYear = 0;
+
+ 		Scanner input = new Scanner(System.in);
+ 		System.out.println("How old are you?");
+
+ 		birthYear = 2024 - input.nextInt();
+ 		
+ 		System.out.println("You were born in " + birthYear);
+ 		input.close();
+ 	}
+ }
