f4519f4904327381eebd390f6c93253f015704b9
Sarocha <rocha.right007@gmail.com>
2024-05-22 16:46:16 -0600

    complete Objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+	public static void main(String[] args){
+		int sum = 0;
+		int counter = 1;
+
+		while(counter <= 20){
+			sum = sum + counter;
+			counter = counter + 1;
+		}
+		System.out.println(sum);
+	}
+}
