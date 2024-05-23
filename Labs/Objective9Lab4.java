a55b607094b077f39b595827e65ede1fa6ea6359
Sarocha <rocha.right007@gmail.com>
2024-05-22 20:46:38 -0600

    Edit the typo

--- Labs/Objective9Lab4.java
+++ Labs/Objective9Lab4.java
@@ -5,13 +5,13 @@ public class Objective9Lab4{
 		Scanner kb = new Scanner(System.in);
 			double num1, num2;
 
-		System.out.println("Please give me a number: ");
+		System.out.print("Please give me a number: ");
 			num1 = kb.nextDouble();
-			System.out.println("Please give me anothr number: ");
+			System.out.print("Please give me anothr number: ");
 			num2 = kb.nextDouble();
 
 	double average = findAverage(num1, num2);
-	System.out.println("The average of " + num1 + " and " + num2 + " is " + average);
+	System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
 
 	kb.close();
 
